806b158f713ec6c5267864e15a68ab0e373c2e7a
Raiska8 <87840244+Raiska8@users.noreply.github.com>
2022-02-12 00:32:11 +0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Любэ - А заря",
+    artist: "ЛЮБЭ",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Любэ - Позови меня тихо по имени",
+    artist: "ЛЮБЭ",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Любэ - Там. за туманами",
+    artist: "ЛЮБЭ",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Любэ - Ты неси меня река",
+    artist: "ЛЮБЭ",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Любэ - Ветер-ветерок",
+    artist: "ЛЮБЭ",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Любэ - Березы",
+    artist: "ЛЮБЭ",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
